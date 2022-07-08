999dc7b34c74e52931c66120ebe2ce8dd4d1c15f
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-07-08 12:23:22 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,45 +1,54 @@
 #include <stdio.h>
 #include <assert.h>
 
-void checkbattery_Temp(float temperature,float soc1, float chargeRate1)
+void checkbattery_status(float temperature,float soc1, float chargeRate1)
 {
-  if(temperature <0 || temperature > 45)
+  int case_number = 0;
+  switch (case_number)
   {
-    printf("Temperature out of range!\n"); 
-  }
-  else
-  {
-     printf("Temperature is in normal range!\n"); 
-    checkbattery_soc(soc1,chargeRate1)
+    case case_number:
+      if(temperature <0 || temperature > 45)
+      {
+        printf("Temperature out of range!\n"); 
+        break;
+      }
+      else
+      {
+        printf("Temperature is in normal range!\n"); 
+        case_number++;
+      }
+      case case_number:
+      if(soc < 20 || soc > 80)
+      {
+        printf("State of Charge out of range!\n");
+        break;
+      }
+      else
+      {
+        printf("soc is in normal range!\n"); 
+        case_number++;
+      }
+    case case_number:
+      if(chargeRate > 0.8) 
+      {
+        printf("Charge Rate out of range!\n");
+        break;
+      }
+      else
+      {
+        printf("charge is in normal range!\n");
+        printf("Battery is in good condition\n");
+        break;
+      }
+      default:
+      printf("Give correct details\n");
+      break;
   }
 }
 
-void checkbattery_soc(float soc, float chargeRate2)
-{
-  if(soc < 20 || soc > 80)
-  {
-    printf("State of Charge out of range!\n");
-  }
-  else
-  {
-     printf("soc is in normal range!\n"); 
-    batteryIsOk(chargeRate2);
-  }
-}
 
-void batteryIsOk(float chargeRate) 
-{
-  if(chargeRate > 0.8) 
-  {
-    printf("Charge Rate out of range!\n");
-  }
-  else
-  {
-    printf("charge is in normal range!\n");
-  }
-}
 
 int main() 
 {
-  checkbattery_Temp(25,70,0.7);
+  checkbattery_status(25,70,0.7);
 }
