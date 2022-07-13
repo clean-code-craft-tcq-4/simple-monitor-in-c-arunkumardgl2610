eba7447b056ee6cbabce019a6fe27c430fa154b8
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-07-13 13:59:33 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,28 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
+#include "parameter_check.h"
 
-int check_Parameter1(float min1, float max1, float Parameter_value1)
-{
-    if(Parameter_value1 < min1 || Parameter_value1 > max1)
-  {
-      return 1;
-  }
-  else
-  {
-    return 0;
-  }
-}
-int check_Parameter2(float max2, float Parameter_value2)
-{
-    if(Parameter_value2 > max2) 
-  {
-    return 1;
-  }
-  else
-  {
-    return 0;
-  }
-}
+
 int batteryIsOk(void) 
 {
   int temperature_status=0, soc_Status=0,charge_status =0;
