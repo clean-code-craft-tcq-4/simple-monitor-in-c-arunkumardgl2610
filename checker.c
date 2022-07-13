9efba484f872d94d001846b18c8d107c4ec1f942
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-07-13 13:54:03 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -14,7 +14,7 @@ int check_Parameter1(float min1, float max1, float Parameter_value1)
 }
 int check_Parameter2(float max2, float Parameter_value2)
 {
-    if(Parameter_value2 > max) 
+    if(Parameter_value2 > max2) 
   {
     return 1;
   }
@@ -27,7 +27,7 @@ void batteryIsOk(void)
 {
   int temperature_status=0, soc_Status=0,charge_status =0;
   temperature_status = check_Parameter1(0,45,25);
-  soc_Status = check_Parameter1(20,80,70)
+  soc_Status = check_Parameter1(20,80,70);
   charge_status = check_Parameter2(0.8,0.7);  
   if(temperature_status == 1 || soc_Status == 1 || charge_status == 1)
   {
