524af8ed09a2f3e9dbda5cb0d709b502a13970d1
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-07-13 13:57:09 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -23,7 +23,7 @@ int check_Parameter2(float max2, float Parameter_value2)
     return 0;
   }
 }
-void batteryIsOk(void) 
+int batteryIsOk(void) 
 {
   int temperature_status=0, soc_Status=0,charge_status =0;
   temperature_status = check_Parameter1(0,45,25);
@@ -31,11 +31,11 @@ void batteryIsOk(void)
   charge_status = check_Parameter2(0.8,0.7);  
   if(temperature_status == 1 || soc_Status == 1 || charge_status == 1)
   {
-    printf("Battery status is not in range!\n");
+    return 1;
   }
   else
   {
-    printf("Battery status is good condition\n");
+    return 0;
   }
 }
 
@@ -43,6 +43,14 @@ void batteryIsOk(void)
 
 int main() 
 {
-  batteryIsOk();
-  
+  int battery_status = 0;
+  battery_status= batteryIsOk();
+  if(battery_status == 1)
+  {
+    printf("Battery status is not in range!\n");
+  }
+  else
+  {
+    printf("Battery status is good condition\n");
+  }  
 }
