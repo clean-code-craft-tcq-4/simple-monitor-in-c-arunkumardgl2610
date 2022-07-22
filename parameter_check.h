2300a7e56614e84fdc86e5a2fdb3c0121499a611
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-07-22 08:40:01 +0530

    Update parameter_check.h

--- parameter_check.h
+++ parameter_check.h
@@ -1,4 +1,4 @@
-int check_Parametertype1(float min1, float max1, float Parameter_value1)
+int checkAgainstMaxMinRange(float min1, float max1, float Parameter_value1)
 {
     if(Parameter_value1 < min1 || Parameter_value1 > max1)
   {
@@ -9,7 +9,7 @@ int check_Parametertype1(float min1, float max1, float Parameter_value1)
     return 0;
   }
 }
-int check_Parametertype2(float max2, float Parameter_value2)
+int checkAgainstMaxRange(float max2, float Parameter_value2)
 {
     if(Parameter_value2 > max2) 
   {
