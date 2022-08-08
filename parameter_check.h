876af130620c59dc304f74b68f7e5fcbdcc4bbaf
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-08-08 11:54:13 +0530

    Update parameter_check.h

--- parameter_check.h
+++ parameter_check.h
@@ -1,6 +1,6 @@
 int checkAgainstMaxMinRange(float min1, float max1, float Parameter_value1)
 {
-    if(Parameter_value1 < min1 || Parameter_value1 > max1)
+   if(Parameter_value1 < min1 || Parameter_value1 > max1)
   {
       return 1;
   }
@@ -19,4 +19,32 @@ int checkAgainstMaxRange(float max2, float Parameter_value2)
   {
     return 0;
   }
+}
+
+int checkAgainstwarnMaxMinRange(float min1, float max1, float Parameter_value1)
+{
+    int war_min = 0,war_max = 0, war_Per=0;
+    war_Per = min1/5;
+    war_min = war_Per + min1;
+    war_max = max1 - war_Per;
+    if( Parameter_value1 <= min1)
+    {
+     printf("Low SOC breach\n");   
+    }
+    if( Parameter_value1 > min1 && Parameter_value1 <= war_min)
+    {
+     printf("LOW_SOC_WARNING\n");   
+    }
+    if( Parameter_value1 > war_min && Parameter_value1 < war_max)
+    {
+     printf("NORMAL\n");   
+    }
+    if( Parameter_value1 >= war_max && Parameter_value1 < max1)
+    {
+     printf("HIGH_SOC_WARNING\n");   
+    }
+    if( Parameter_value1 > max1)
+    {
+     printf("HIGH_SOC_BREACH\n");   
+    }
 }
