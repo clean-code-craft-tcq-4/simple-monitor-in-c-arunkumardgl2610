4a1cb61e7d08dda2a862616e2bb289c5d2beb7af
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-08-08 17:18:28 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include "parameter_check.h"
 int temperature_status=0, soc_Status=0,charge_status =0,battery_status=0;
-int batteryIsInGoodCondition(float para1,float para2,float para3) 
+int batteryIsInGoodCondition(float para1,float para2,float para3,char lang) 
 {
   temperature_status = checkAgainstMaxMinRange(0,45,para1);
   checkAgainstwarnMaxMinRange(20,80,para2);
@@ -10,21 +10,36 @@ int batteryIsInGoodCondition(float para1,float para2,float para3)
   battery_status = (temperature_status | charge_status | soc_Status);
   if(battery_status==1)
   {
-    printf("Battery status is not in range!\n");
+    if(lang == 1)
+    {
+      printf("Battery status is not in range!\n");
+    }
+    else if(lang == 2)
+    {
+      printf("Batteriestatus ist nicht im Bereich!\n");
+    }
     return 0;
   }
   else
   {
-    printf("Battery status is good condition\n");
+    if(lang == 1)
+    {
+      printf("Battery status is good condition\n");
+    }
+    else if(lang == 2)
+    {
+      printf("Batteriestatus ist guter Zustand\n");
+    }
+    
     return 1;
   }
 }
 int main() 
 {
-  assert(batteryIsInGoodCondition(25, 70, 0.7));
-  assert(!batteryIsInGoodCondition(50, 85, 0));
-  assert(!batteryIsInGoodCondition(60, 15, 0.9));
-  assert(batteryIsInGoodCondition(25, 40, 0.4));
-  assert(!batteryIsInGoodCondition(43, 78, 0.9));
-  assert(batteryIsInGoodCondition(25, 79, 0.6));
+  assert(batteryIsInGoodCondition(25, 70, 0.7,1));
+  assert(!batteryIsInGoodCondition(50, 85, 0,2));
+  assert(!batteryIsInGoodCondition(60, 15, 0.9,1));
+  assert(batteryIsInGoodCondition(25, 40, 0.4,2));
+  assert(!batteryIsInGoodCondition(43, 78, 0.9,1));
+  assert(batteryIsInGoodCondition(25, 79, 0.6,2));
 }
