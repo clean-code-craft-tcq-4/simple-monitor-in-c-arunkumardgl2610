3f45e9118f898f58039b0d6fbaf53620600bd7a5
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-08-23 12:20:07 +0530

    Update parameter_check.h

--- parameter_check.h
+++ parameter_check.h
@@ -1,4 +1,4 @@
-extern lang=0;
+
 int checkAgainstMaxMinRange(float min1, float max1, float Parameter_value1)
 {if(Parameter_value1 < min1 || Parameter_value1 > max1)
   {return 1;}
@@ -9,12 +9,12 @@ int checkAgainstMaxRange(float max2, float Parameter_value2)
   {return 1;}
   else
   {return 0;}}
-int display_status(int battery_status_display)
+int display_status(int battery_status_display,int language)
 {if(battery_status_display == 1) 
-  {if(lang == 1){printf("Battery status is not in range!\n");}
+  {if(language == 1){printf("Battery status is not in range!\n");}
     else{printf("Batteriestatus ist nicht im Bereich!\n");}
     return 0;}
   else{
-    if(lang == 1){printf("Battery status is good condition\n");}
+    if(language == 1){printf("Battery status is good condition\n");}
     else{printf("Batteriestatus ist guter Zustand\n"); }
     return 1;}}
