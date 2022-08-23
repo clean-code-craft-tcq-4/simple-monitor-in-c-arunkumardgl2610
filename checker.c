0058be59c876a8c947cfb48a807dc850685fdd11
arunkumardgl2610 <105204463+arunkumardgl2610@users.noreply.github.com>
2022-08-23 12:13:29 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,33 +1,29 @@
 #include <stdio.h>
 #include <assert.h>
 #include "parameter_check.h"
-int temperature_status=0, soc_Status=0,charge_status =0,battery_status=0,lang=0;
+int temperature_status=0, soc_Status=0,charge_status =0,battery_status=0,lang=0,overall_status;
 int batteryIsInGoodCondition(float para1,float para2,float para3) 
 {
   temperature_status = checkAgainstMaxMinRange(0,45,para1);
   soc_Status = checkAgainstMaxMinRange(20,80,para2);
   charge_status = checkAgainstMaxRange(0.8,para3);
   battery_status = (temperature_status | charge_status | soc_Status);
-  display_status(battery_status);
-return battery_status;
+  overall_status = display_status(battery_status);
+  return overall_status;
 }
-void display_status(int battery_status_display)
-{
-  if(battery_status_display == 1) 
-  {
-    if(lang == 1)
-    printf("Battery status is not in range!\n");
-    else
-    printf("Batteriestatus ist nicht im Bereich!\n"); 
-    
-  }
-  else
-  {
-    if(lang == 1)
-    printf("Battery status is good condition\n");
-    else
-    printf("Batteriestatus ist guter Zustand\n"); 
-  }
+int display_status(int battery_status_display)
+{if(battery_status_display == 1) 
+  {if(lang == 1){
+    printf("Battery status is not in range!\n");}
+    else{
+    printf("Batteriestatus ist nicht im Bereich!\n");}
+    return 0;}
+  else{
+    if(lang == 1){
+    printf("Battery status is good condition\n");}
+    else{
+    printf("Batteriestatus ist guter Zustand\n"); }
+    return 1;}
 }
 int main() 
 {
